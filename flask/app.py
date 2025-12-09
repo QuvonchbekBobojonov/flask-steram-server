6e019cad323d72642217723368dc13dfca00f02a
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-12-10 01:22:18 +0500

    ui: add test u\page

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, jsonify, request, session
-from livekit import AccessToken, VideoGrants
+from livekit import api
 import time
 
 app = Flask(__name__)
-app.secret_key = "q=lA=VS@Z`:@%El;Y*e**sL:w&Z?["
+app.secret_key = "tw5e325efeg2673rf273r6f7236r"
 
 ADMIN_PASSWORD = "moorfo_12345"
 
-LIVEKIT_URL = "ws://livekit:7880"
+LIVEKIT_URL = "wss://ic3-camera-cantrol.moorfo.uz/livekit"  # NGINX orqali
 LIVEKIT_API_KEY = "API4KJnqvB4ggR6"
 LIVEKIT_API_SECRET = "88dkD9EwafTA20mYmFOn8q7sl9FSCMBJc0wGBjYHa1N"
 ROOM = "exam"
@@ -16,7 +16,8 @@ last_seen = {}
 
 @app.post("/auth")
 def auth():
-    if request.get_json().get("password") == ADMIN_PASSWORD:
+    data = request.get_json(force=True, silent=True) or {}
+    if data.get("password") == ADMIN_PASSWORD:
         session["admin"] = True
         return jsonify(ok=True)
     return jsonify(ok=False), 401
@@ -24,12 +25,24 @@ def auth():
 @app.get("/token/<username>")
 def token(username):
     last_seen[username] = time.time()
-    at = (
-        AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET)
-        .with_identity(username)
-        .with_grants(VideoGrants(room_join=True, room=ROOM))
+
+    at = api.AccessToken(
+        LIVEKIT_API_KEY,
+        LIVEKIT_API_SECRET
+    ).with_identity(username).with_grants(
+        api.VideoGrants(
+            room_join=True,
+            room=ROOM,
+            can_publish=True,
+            can_subscribe=True
+        )
     )
-    return jsonify(token=at.to_jwt(), url=LIVEKIT_URL, room=ROOM)
+
+    return jsonify(
+        token=at.to_jwt(),
+        url=LIVEKIT_URL,
+        room=ROOM
+    )
 
 @app.get("/update")
 def update():
@@ -46,4 +59,5 @@ def index():
 def test_camera():
     return render_template("test-camera.html")
 
-app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
